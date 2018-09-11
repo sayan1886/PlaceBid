224eccd914830597332c154985bdf86d5740cb58
Sayan Chatterjee <sayan1886@gmail.com>
2018-09-11 23:40:41 +0530

    Removed Build dir

--- contracts/AuctionRecord.sol
+++ contracts/AuctionRecord.sol
@@ -24,8 +24,6 @@ contract AuctionRecord {
         uint8 currentBid; //in amount
         string currentBidder; //Owner and Biider will not be same
 
-        Bid[] bids;
-
         AuctionStatus status;
         AuctionResult result;
     }
@@ -52,11 +50,6 @@ contract AuctionRecord {
         _;
     }
     
-    modifier validIndex (uint8 _auctionId, uint8 index) {
-        require (index < auctions[_auctionId].bids.length);
-        _;
-    }
-    
     function createAuction ( uint8 _assetId, string _assetName, string _assetOwner, uint8 _startPrice , uint256 _auctionExpiry) public payable returns (uint8 _auctionID) {
         require(bytes(auctions[_auctionId].auctionHash).length == 0);
         uint8 _auctionId = ++id;
@@ -74,7 +67,6 @@ contract AuctionRecord {
     function placeBid (uint8 _auctionId, uint8 _bid, string _bidder) validAuction(_auctionId) auctionActive(_auctionId) highestBid(_auctionId, _bid) public payable returns (bool _bidPlaced){
         auctions[_auctionId].currentBid = _bid;
         auctions[_auctionId].currentBidder = _bidder;
-        auctions[_auctionId].bids.push(Bid(_bidder, _bid));
         return true;
     }
     
@@ -82,7 +74,7 @@ contract AuctionRecord {
         return id;
     }
 
-    function getAssetID () public view returns (uint8 _auctionId){
+    function getAssetID (uint8 _auctionId) public view returns (uint8 _assetId){
         return auctions[_auctionId].assetId;
     }
 
@@ -90,7 +82,7 @@ contract AuctionRecord {
         return auctions[_auctionId].assetOwner;
     }
     
-    function getAsset (uint8 _auctionId) validAuction(_auctionId) public view returns (string _assetName) {
+    function getAssetName (uint8 _auctionId) validAuction(_auctionId) public view returns (string _assetName) {
         return auctions[_auctionId].assetName;
     }
     
@@ -100,18 +92,6 @@ contract AuctionRecord {
     
     function getCurrentBidder (uint8 _auctionId) validAuction(_auctionId) public view returns (string _currentBidder) {
         return auctions[_auctionId].currentBidder;
-    }
-    
-    function getTotalBids (uint8 _auctionId) validAuction(_auctionId) public view returns (uint _bids) {
-        return auctions[_auctionId].bids.length;
-    }
-    
-    function getBidderAtIndex(uint8 _auctionId, uint8 index) validAuction (_auctionId) validIndex (_auctionId, index) public view returns (string _bidder) {
-        return auctions[_auctionId].bids[index].bidder;
-    }
-    
-    function getBidAtIndex(uint8 _auctionId, uint8 index) validAuction (_auctionId) validIndex (_auctionId, index) public view returns (uint8 _bid) {
-        return auctions[_auctionId].bids[index].bid;
     }
     
     function getAuctionStatus (uint8 _auctionId) validAuction (_auctionId) public view returns (AuctionStatus _auctionStatus) {
